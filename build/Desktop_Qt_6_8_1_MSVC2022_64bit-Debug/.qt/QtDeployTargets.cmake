d66e468e7d24c70c052574b4d6420fe4f0f2fbb8
MTsocute <75737534+MTsocute@users.noreply.github.com>
2025-01-07 01:09:35 +0800

    删除了编译文件

--- build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_SerialHelper_FILE D:/Code/QT/SerialHelper/build/Desktop_Qt_6_8_1_MSVC2022_64bit-Debug/SerialHelper.exe)
-set(__QT_DEPLOY_TARGET_SerialHelper_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_SerialHelper_RUNTIME_DLLS D:/App/QT/6.8.1/msvc2022_64/bin/Qt6Widgetsd.dll;D:/App/QT/6.8.1/msvc2022_64/bin/Qt6SerialPortd.dll;D:/App/QT/6.8.1/msvc2022_64/bin/Qt6Guid.dll;D:/App/QT/6.8.1/msvc2022_64/bin/Qt6Cored.dll)
